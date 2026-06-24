0da4da4f83ee9841cc3166f409cf67aa9b579950
pawunder1992 <pavelkryvutsa@gmail.com>
2026-06-24 19:50:03 +0300

    resolved

--- app/parse.py
+++ app/parse.py
@@ -1,6 +1,12 @@
 from dataclasses import dataclass
 
 
+import requests
+from bs4 import BeautifulSoup
+
+BASE_URL = "https://mate.academy/"
+
+
 @dataclass
 class Course:
     name: str
@@ -8,5 +14,19 @@ class Course:
     duration: str
 
 
+def get_single_course(course: Course) -> Course:
+    return Course(
+        name=course.select_one(".ProfessionCard_title__m7uno").text,
+        short_description=course.select_one(
+            ".ProfessionCard_description__K8weo"
+        ).text,
+        duration=course.select_one(".ProfessionCard_text___l0Du").text,
+    )
+
+
 def get_all_courses() -> list[Course]:
-    pass
+
+    html_content = requests.get(BASE_URL).content
+    soup = BeautifulSoup(html_content, "html.parser")
+    courses = soup.select(".ProfessionCard_content__mPiVi")
+    return [get_single_course(course) for course in courses]
